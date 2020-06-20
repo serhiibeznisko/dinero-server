b3baacea478b4951bae81f8a7cf9ee37de54aece
serhiicodeaddict <serhii@codeaddict.io>
2020-06-20 23:28:27 +0200

    Change production_startup.sh

--- production_startup.sh
+++ production_startup.sh
@@ -3,4 +3,4 @@ cd src
 python manage.py collectstatic --noinput
 python manage.py migrate
 touch test.ttt
-#gunicorn --bind=0.0.0.0 --timeout 600 config.wsgi:application
+gunicorn --bind=0.0.0.0 --timeout 600 config.wsgi:application
